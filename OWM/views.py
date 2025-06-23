4fd03fa4dbb1679b356f578142159de6986f7ee8
Bonfeb <bonfebdevs@gmail.com>
2025-06-23 14:57:26 +0300

    video post

--- OWM/views.py
+++ OWM/views.py
@@ -694,6 +694,7 @@ class VideoView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        print("Video File:", request.FILES)
         serializer = VideoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
