b39cb7dfbc62f0542fae2e2ee2e02b7fbb7b7b41
Bonfeb <bonfebdevs@gmail.com>
2025-07-04 19:14:45 +0300

    fixing pending instead of unpaid status

--- staticfiles/admin/js/vendor/jquery/jquery.js
+++ staticfiles/admin/js/vendor/jquery/jquery.js
@@ -3331,7 +3331,7 @@ jQuery.Callbacks = function( options ) {
 
 			// Disable .fire
 			// Also disable .add unless we have memory (since it would have no effect)
-			// Abort any pending executions
+			// Abort any  executions
 			lock: function() {
 				locked = queue = [];
 				if ( !memory && !firing ) {
